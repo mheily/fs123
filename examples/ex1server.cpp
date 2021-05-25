a3dc80570e5364bb4ef602709217b3d492458f43
johnsalmon <salmonj@deshawresearch.com>
2021-05-25 11:11:36 -0400

    Simplify and improve the example server
    Remove the threading and the "fancy" synthetic generation of
    data (that's all in testserver/ now).
    
    Show how to reply to readdir and symlink requests.
    
    Change-Id: Id60a02759bda8234e997b839e659a2fa5651e118

--- examples/ex1server.cpp
+++ examples/ex1server.cpp
@@ -1,9 +1,13 @@
 #include "fs123/fs123server.hpp"
-#include <core123/svto.hpp>
 #include <core123/opt.hpp>
-#include <core123/strutils.hpp>
+#include <core123/diag.hpp>
+#include <sys/stat.h>
+#include <unistd.h>
 
 using namespace core123;
+using namespace std;
+
+auto _ex1server = diag_name("ex1server");
 
 // Nothing ever changes, so we can use the same validator,
 // estale_cookie and etag everywhere.  There's no reason
@@ -13,63 +17,75 @@ static constexpr uint64_t validator = 12345;
 static constexpr uint64_t estale_cookie = 54321;
 static constexpr uint64_t etag = 31415;
 
-struct bench_handler: public fs123p7::handler_base{
+struct example_handler: public fs123p7::handler_base{
     // N.B.  The cc could be a constructor-argument...
-    std::string cc = "max-age=3600,stale-while-revalidate=7200,stale-if-error=86400";
+    string cc = "max-age=3600,stale-while-revalidate=7200,stale-if-error=86400";
     bool strictly_synchronous() override { return true; }
     void a(fs123p7::req::up reqp) override {
+        DIAGf(_ex1server, "a(%s)", string(reqp->path_info).c_str());
         struct stat sb = {}; // all zeros!
-        if(reqp->path_info.empty()){
-            // asking about the root.  It's executable but niether
-            // readable nor writable.
-            sb.st_mode = S_IFDIR | 0111;
-        }else{
-            // Other than the root, the only entries are regular files
-            // whose names can be parsed as numbers.
+        sb.st_uid = geteuid();
+        sb.st_gid = getegid();
+        // st_mtime, st_ctime, etc.  Leave them at zero?  Set them to the current time?
+        // Some other time?  
+        if(reqp->path_info == ""){
+            // the root of the filesystem
+            sb.st_mode = S_IFDIR | 0555;
+            sb.st_nlink = 2;
+        }else if(reqp->path_info == "/hello"){
+            // the file called "hello"
             sb.st_mode = S_IFREG | 0444;
-            try{
-                sb.st_size = svto<size_t>(reqp->path_info, 1);
-            }catch(std::exception&){
-                return errno_reply(std::move(reqp), ENOENT, cc);
-            }
+            sb.st_nlink = 1;
+            sb.st_size = 6; // "world\n"
+        }else if(reqp->path_info == "/hi"){
+            // the symlink called "hi"
+            sb.st_mode = S_IFLNK | 0777;
+            sb.st_nlink = 1;
+            sb.st_size = 5; // "hello"
+        }else{
+            return errno_reply(move(reqp), ENOENT, cc);
         }
-        a_reply(std::move(reqp), sb, validator, estale_cookie, cc);
+        a_reply(move(reqp), sb, validator, estale_cookie, cc);
     }
     void d(fs123p7::req::up reqp, uint64_t /*inm64*/, bool /*begin*/, int64_t /*offset*/) override{
-        // There are files here, but you can't list them.
-        d_reply(std::move(reqp), true, validator, estale_cookie, cc);
+        DIAGf(_ex1server, "d(%s)", string(reqp->path_info).c_str());
+        if(reqp->path_info != "")
+            return errno_reply(move(reqp), ENOTDIR, cc);
+        reqp->add_dirent("hello", 1, DT_REG, estale_cookie);
+        reqp->add_dirent("hi", 2, DT_LNK, estale_cookie);
+        reqp->add_dirent(".", 3, DT_DIR, estale_cookie);
+        reqp->add_dirent("..",  4, DT_DIR, estale_cookie);
+        d_reply(move(reqp), true, validator, estale_cookie, cc);
     }
     void f(fs123p7::req::up reqp, uint64_t inm64, size_t len, uint64_t offset, void* buf) override{
+        DIAGf(_ex1server, "f(%s)", string(reqp->path_info).c_str());
+        if(reqp->path_info != "/hello")
+            return errno_reply(move(reqp), ENOENT, cc);
         if(inm64 == etag)
-            return not_modified_reply(std::move(reqp), cc);
-        size_t sz;
-        // The only files that exist have names that can be parsed as
-        // numbers.  Their contents is the letter 'x' repeated
-        // <filename> times.
-        try{
-            sz = svto<size_t>(reqp->path_info, 1);
-        }catch(std::exception&){
-            return errno_reply(std::move(reqp), ENOENT, cc);
-        }
+            return not_modified_reply(move(reqp), cc);
+        size_t sz = sizeof("world\n")-1; // exclude terminal NUL
         size_t n;
         if(offset > sz)
             n = 0;
-        else if(len + offset > sz)
+        else if(len > sz-offset)
             n = sz - offset;
         else
             n = len;
-        ::memset(buf, 'x', n);
-        f_reply(std::move(reqp), n, validator, etag, estale_cookie, cc);
+        ::memcpy(buf, "world\n" + offset, n);
+        f_reply(move(reqp), n, validator, etag, estale_cookie, cc);
     }
     void l(fs123p7::req::up reqp) override{
-        errno_reply(std::move(reqp), ENOENT, cc);
+        DIAGf(_ex1server, "l(%s)", string(reqp->path_info).c_str());
+        if(reqp->path_info != "/hi")
+            return errno_reply(move(reqp), ENOENT, cc);
+        l_reply(move(reqp), "hello", cc);
     }
     void s(fs123p7::req::up reqp) override{
-        errno_reply(std::move(reqp), ENOTSUP, cc);
+        errno_reply(move(reqp), ENOTSUP, cc);
     }
 
-    bench_handler(){}
-    ~bench_handler(){}
+    example_handler() = default;
+    ~example_handler() = default;
 };
 
 int main(int argc, char *argv[]) try
@@ -81,38 +97,24 @@ int main(int argc, char *argv[]) try
     // Add a few more options of our own:
     bool help=false;
     opt_parser.add_option("help", "produce this message", opt_true_setter(help));
-    int threadpool_max;
-    opt_parser.add_option("threadpool_max", "0", "maximum number of handler threads", opt_setter(threadpool_max));
-    int threadpool_idle;
-    opt_parser.add_option("threadpool_idle", "1", "number of handler threads at zero load", opt_setter(threadpool_idle));
 
     // Parse all options together, populating both server_opts and
     // our own options.
     auto more_args = opt_parser.setopts_from_argv(argc, argv);
     // Help only?
     if(help){
-        std::cerr << opt_parser.helptext() << "\n";
+        cerr << opt_parser.helptext() << "\n";
         return 0;
     }
     if(!more_args.empty())
-        throw std::runtime_error("unrecognized arguments:" + strbe(more_args));
+        throw runtime_error("unrecognized arguments:" + strbe(more_args));
 
-    // Create a bench_handler, optionally wrap it with a threadpool wrapper,
-    // and then create a server with the handler.
-    bench_handler h;
-    std::unique_ptr<fs123p7::tp_handler<bench_handler>> tph;
-    std::unique_ptr<fs123p7::server> s;
-    if(threadpool_max){
-        tph = std::make_unique<fs123p7::tp_handler<bench_handler>>(threadpool_max,
-                                                                   threadpool_idle, h);
-        s = std::make_unique<fs123p7::server>(server_opts, *tph);
-    }else{
-        s = std::make_unique<fs123p7::server>(server_opts, h);
-    }
-    // run the server (forever - until we get kill()-ed)
-    s->run();
+    // Construct a handler, a server, and call run().
+    example_handler h;
+    fs123p7::server s(server_opts, h);
+    s.run();
     return 0;
- }catch(std::exception& e){
+ }catch(exception& e){
     core123::complain(e, "Shutting down because of exception caught in main");
     return 1;
  }
